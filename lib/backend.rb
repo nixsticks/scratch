3765b76de26622ae21aaab736c70ddfefa0c51fe
Nikki Thean <nikki.thean@gmail.com>
2014-01-03 20:51:03 -0500

    Update stuffs

--- lib/backend.rb
+++ lib/backend.rb
@@ -20,25 +20,18 @@ class Backend
     @clients = []
 
     Thread.new do
-      TweetStream::Client.new.on_inited do
-      end.on_enhance_your_calm do
+      client = TweetStream::Client.new
+
+      client.track('clapforashley') do |status|
+        get_status(status)
+      end
+
+      client.follow(304067888) do |status|
+        get_status(status)
+      end
+
+      client.on_enhance_your_calm do
         puts "shit i hit the fucking rate limit"
-      end.on_status_withheld do
-        puts "shit status withheld"
-      end.on_error do |message|
-        puts message
-      end.follow(304067888) do |status|
-        puts "I GOT A STATUS"
-        status = "#{status.text} - @#{status.user.screen_name}"
-        MUTEX.synchronize {
-          @clients.each {|client| client.send(JSON.dump status)}
-        }
-      end.track("clapforashley") do |status|
-        puts "I GOT A STATUS"
-        status = "#{status.text} - @#{status.user.screen_name}"
-        MUTEX.synchronize {
-          @clients.each {|client| client.send(JSON.dump status)}
-        }
       end
     end
   end
@@ -67,6 +60,13 @@ class Backend
     else
       @app.call(env)
     end
+  end
+
+  def get_status(status)
+    message = "#{status.text} - @#{status.user.screen_name}"
+    MUTEX.synchronize {
+      @clients.each {|client| client.send(JSON.dump message)}
+    }
   end
 end
 
